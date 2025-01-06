91e3b4dc625d102bd4ef406342d576e69fbf2243
jorritvanderheide <35707261+jorritvanderheide@users.noreply.github.com>
2025-01-06 11:51:35 +0100

    feat: add spicetify theme

--- modules/home/features/spotify.nix
+++ modules/home/features/spotify.nix
@@ -3,13 +3,20 @@
   config,
   inputs,
   lib,
+  pkgs,
   ...
-}: {
+}: let
+  spicePkgs = inputs.spicetify-nix.packages.${pkgs.system}.default;
+in {
   imports = [
     inputs.spicetify-nix.homeManagerModules.default
   ];
 
-  programs.spicetify.enable = true;
+  programs.spicetify = {
+    enable = true;
+    theme = spicePkgs.themes.catppuccin;
+    colorScheme = "mocha";
+  };
 
   # Conditionally persist directories
   myHome = lib.mkIf config.myHome.impermanence.enable {
