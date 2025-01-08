d29e97aaf3bb64f75ae65b02bd6c8becac520038
jorritvanderheide <35707261+jorritvanderheide@users.noreply.github.com>
2025-01-08 07:41:43 +0100

    feat: add vesktop theme

--- modules/home/features/discord.nix
+++ modules/home/features/discord.nix
@@ -9,10 +9,15 @@
     vesktop
   ];
 
+  xdg.configFile."vesktop/themes/modular.theme.css".text = ''
+    @import url("https://github.com/SEELE1306/Modular/blob/release/modular.theme.css")
+  '';
+
   # Conditionally persist directories
   myHome = lib.mkIf config.myHome.impermanence.enable {
     impermanence.directories = [
-      ".config/vesktop"
+      ".config/vesktop/sessionData"
+      ".config/vesktop/settings"
     ];
   };
 }
