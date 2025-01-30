0eb0d13f864cd4acbe065310e7a839275daada1f
jorritvanderheide <35707261+jorritvanderheide@users.noreply.github.com>
2025-01-30 20:20:15 +0100

    fix: clean up discord

--- modules/home/features/discord.nix
+++ modules/home/features/discord.nix
@@ -6,19 +6,13 @@
   ...
 }: {
   home.packages = with pkgs; [
-    discord
     vesktop
   ];
-
-  xdg.configFile."vesktop/themes/modular.theme.css".text = ''
-    @import url("https://github.com/SEELE1306/Modular/blob/release/modular.theme.css")
-  '';
 
   # Conditionally persist directories
   myHome = lib.mkIf config.myHome.impermanence.enable {
     impermanence.directories = [
-      ".config/vesktop/sessionData"
-      ".config/vesktop/settings"
+      ".config/vesktop"
     ];
   };
 }
