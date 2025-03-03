deb109d57721dff4e929cfa5d0aedab81b7ef451
jorritvanderheide <35707261+jorritvanderheide@users.noreply.github.com>
2025-03-03 13:03:15 +0100

    feat: fix wallpaper switch

--- modules/home/features/hyprland/scripts/set-wallpaper.sh
+++ modules/home/features/hyprland/scripts/set-wallpaper.sh
@@ -3,42 +3,51 @@
 # Define the paths to your wallpapers
 LIGHT_WALLPAPER="$HOME/Pictures/Backgrounds/wallpaper-light.jpg"
 DARK_WALLPAPER="$HOME/Pictures/Backgrounds/wallpaper-dark.jpg"
+MODE_FILE="$HOME/.current_wallpaper_mode"
 
 # Get the current hour
 HOUR=$(date +"%H")
 
-# # Check for command-line argument
-# if [ "$1" = "toggle" ]; then
-#   # Read the current wallpaper mode from a file
-#   if [ -f "$HOME/.current_wallpaper_mode" ]; then
-#     CURRENT_MODE=$(cat "$HOME/.current_wallpaper_mode")
-#   else
-#     CURRENT_MODE="light"
-#   fi
+# Script to change the wallpaper
+set_wallpaper() {
+  if [ "$MODE" = "light" ]; then
+    printf "Setting light wallpaper\n"
+    swww img "$LIGHT_WALLPAPER"
+    # swww img -o "DP-1" "$LIGHT_WALLPAPER"
+  else
+    printf "Setting dark wallpaper\n"
+    swww img "$DARK_WALLPAPER"
+    # swww img -o "DP-1" "$DARK_WALLPAPER"
+  fi
+}
 
-#   # Toggle the wallpaper mode
-#   if [ "$CURRENT_MODE" = "light" ]; then
-#     MODE="dark"
-#   else
-#     MODE="light"
-#   fi
+# Handle toggle mode
+if [ "$1" = "toggle" ]; then
+  if [ -f "$MODE_FILE" ]; then
+    CURRENT_MODE=$(cat "$MODE_FILE")
+  else
+    CURRENT_MODE="light"
+  fi
 
-#   # Save the new mode to the file
-#   echo "$MODE" > "$HOME/.current_wallpaper_mode"
-# else
-#   # Set the wallpaper based on the time of day if no argument is provided
-#   if [ "$HOUR" -ge 6 ] && [ "$HOUR" -lt 18 ]; then
-#     MODE="light"
-#   else
-#     MODE="dark"
-#   fi
-# fi
+  # Toggle the mode
+  if [ "$CURRENT_MODE" = "light" ]; then
+    MODE="dark"
+  else
+    MODE="light"
+  fi
 
-# Set the wallpaper based on the time of day
-if [ "$HOUR" -ge 6 ] && [ "$HOUR" -lt 18 ]; then
-  swww img -o "eDP-1" "$LIGHT_WALLPAPER"
-  swww img -o "DP-1" "$LIGHT_WALLPAPER"
+  echo "$MODE" > "$MODE_FILE"
+
 else
-  swww img -o "eDP-1" "$DARK_WALLPAPER"
-  swww img -o "DP-1" "$DARK_WALLPAPER"
+  # Set mode based on time if no argument is provided
+  if [ "$HOUR" -ge 6 ] && [ "$HOUR" -lt 18 ]; then
+    MODE="light"
+  else
+    MODE="dark"
+  fi
 fi
+
+# Apply wallpaper
+echo "$MODE" > "$MODE_FILE"
+printf "$MODE"
+set_wallpaper
--- modules/system/features/hyprland.nix
+++ modules/system/features/hyprland.nix
@@ -13,6 +13,7 @@
   };
 
   environment.systemPackages = with pkgs; [
+    bc
     brightnessctl
     cliphist
     dunst
